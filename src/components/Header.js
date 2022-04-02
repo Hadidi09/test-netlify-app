d56d6bddc6c0db3bf1802b3ab4054253dcbe1fad
assosuffrage <assosuffrage@gmail.com>
2022-04-02 11:32:29 +0200

    deploy

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <p>Home</p>
+            <p>panier</p>
+        </div>
+    );
+};
+
+export default Header;
